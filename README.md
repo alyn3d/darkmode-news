74d5105fc3ce6e8d76e5f9501943e301984bf89f
alyn3d <alyn3d@gmail.com>
2021-04-12 00:25:50 +0300

    feat: :sparkles: Initial commit. V0.1

---
+++ README.md
@@ -0,0 +1,39 @@
+# DarkMode News Chrome Extension
+
+Quick and dirty Chrome extension to force some news websites to go into dark mode.
+
+### Installing
+
+Download the current release from Github.
+In Chrome open "Extensions" and enable "Developer Mode".
+Restart Chrome
+Drag and drop the .crx file into Chrome Extensions
+
+# Release
+
+v0.1
+
+### Websites currently supported
+
+- digi24.ro
+- theguardian.com
+- reuters.com
+- bbc.com
+- foreignaffairs.com
+- financialtimes.com
+- chicagotribune.com
+- apnews.com
+- europa.eu
+- dw.com
+
+## Author
+**Alin Ion** - [alyn3d](https://github.com/alyn3d)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## TODO
+
+* Add more supported websites 
+* Improve dark mode styling
---
+++ src/run.js
@@ -0,0 +1,15 @@
+document.body.style.backgroundColor = '#121212';
+var bodyElements = document.querySelectorAll("html, .content, .content-main, .content-right, #layout-body, .nav-collapse, .main, .webLinContainer-0-2-121, .TopStories, .Body, .crd, .n-content-layout[data-layout-name=card], .package--basic.package__nav--end, .n-content-body, .o-header__drawer-menu-list, .o-teaser--hero, .o-header__container, .o-teaser__content, div#navContainer, .news, div#bodyMover, div.imgTeaserM.below.white, div.teaserContentWrap, div#bodyContent, .teaserContentWrap, nav, #orb-header, .light-grey-bg, .messages--container, .article-header--metadata-title, .lead-story-most-read, .home-stripe, .homepage--sponsor-content, .home-section, .header-navbar-link, .nw-c-slice-heading__title, .nw-c-slice-heading__action-text, .gs-c-promo-body, footer, header, .product, .news-tips, .feature__title, .block-link, .subnav, .column2, #rcs-mainContentTop, #rcs-mainContentBottom, .fc-item, section, .section, .bg, .widget-blog, .widget, .bg-gradient, .bg-white, .container-white");
+var j;
+for (j = 0; j < bodyElements.length; j++) {
+  bodyElements[j].style.background = "transparent";
+  bodyElements[j].style.setProperty("background-color", "#121212", "important");
+}
+var elements = document.querySelectorAll("body, h1, h2, h3, h4, h5, h6, p, a, span, .fc-item__header, .fc-item__standfirst, .book-review-homepage--summary, .tb-megamenu, .dropdown-menu, .ecl-paragraph");
+var i;
+for (i = 0; i < elements.length; i++) {
+  elements[i].style.color = "#FFFFFF";
+}
+
+var transparentElements = document.querySelectorAll('.media--overlay');
+transparentElements.style.setProperty('background-color', 'rgba(0,0,0,0.5)', 'important');
